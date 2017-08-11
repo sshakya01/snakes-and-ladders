32ab711b219731c0df08c2927efdc1bdedd1da3d
Sajil Shakya <shakya.sajil@gmail.com>
2017-08-11 10:34:47 -0400

    added turns

--- js/dice.js
+++ js/dice.js
@@ -1,16 +1,10 @@
 
 function rollDice(){
     var die = document.getElementById("die");
-    var d1 = Math.floor(Math.random() * 6) + 1;
+    var d1 = Math.floor(Math.random() * 6) + 1;// generating random no. from 1-6
     console.log(d1);
     die.innerHTML = d1;
     // return die;
     console.log('what we rolled --> ', d1);
     move(d1);
 }
-
-// var min = 1;
-// var max = 6;
-// function randomInt(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
--- js/game.js
+++ js/game.js
@@ -1,20 +1,22 @@
 console.log("gamejs loaded");
 
 
-//let player1turn = true;
+let player1turn = true;
 const player1=document.querySelector("#red");
-//const player2=document.querySelector("#blue");
+const player2=document.querySelector("#blue");
 const die=document.getElementById("die");
 //let cells = document.querySelectorAll('.cell')
 var playerPosition1 = 0;
-// var playerPosition2 = 0;
+var playerPosition2 = 0;
 document.addEventListener("click",function(){
-//   turns +=1
-move(die);
+
+//move(die);
 });
 
 function move(dieroll){
   // first remove the X from the current one where you are
+
+if(player1turn==true){
 let whereIwas = document.getElementById(playerPosition1);
 if(playerPosition1 !== 0) {
 
@@ -27,5 +29,22 @@ if(playerPosition1 !== 0) {
   let whereToGo = document.getElementById(playerPosition1);
   whereToGo.innerHTML = 'X';
   //cells[playerPosition].innerHTML= player1;
+player1turn=false;
+}
 
+else{
+let whereIwas2 = document.getElementById(playerPosition2);
+if(playerPosition2 !== 0) {
+
+  whereIwas2.innerHTML = playerPosition2;
+    // then add the dice roll to the player postition
+}
+  playerPosition2 += dieroll;
+  console.log('added the dice roll -> ',playerPosition2);
+  // change the corresponding div to the new player holder
+  let whereToGo2 = document.getElementById(playerPosition2);
+  whereToGo2.innerHTML = 'P2';
+  //cells[playerPosition].innerHTML= player1;
+player1turn=true;
+}
 }
